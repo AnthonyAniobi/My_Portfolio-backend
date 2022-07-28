a79b3fffe8a5fb0626871b097d52b859a0976d8b
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-07-28 10:43:19 +0100

    added css to html

--- web/models/category.py
+++ web/models/category.py
@@ -3,3 +3,6 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=250, verbose_name='Name of Category')
     description= models.TextField(default='no description', verbose_name='Description of Category')
+
+    def __str__(self) -> str:
+        return self.name
--- web/views.py
+++ web/views.py
@@ -4,17 +4,6 @@ from web.mock_data import icons_contact, personal_info, skills
 
 def index(request):
 
-    #     personal_info=personal_info.PersonalInfo(),
-    #     codeSkills=skills.MySkills.getCoding(),
-    #     designSkills=skills.MySkills.getDesign(),
-    #     education=education.Education.getAll(),
-    #     experience=experience.Experience.getAll(),
-    #     works=works.MyWorks.getAll(),
-    #     worksCategory = works.MyWorks.categories(),
-    #     icons=IconsContact,
-    #     getmore=works.MyWorks.getMore()
-    #     )
-
     context = {
         "personal_info":personal_info.PersonalInfo(),
         "codeSkills":skills.MySkills.getCoding(),
@@ -23,5 +12,13 @@ def index(request):
         'experience': experience.Experience.objects.all(),
         'works': work.Work.objects.all(),
         'worksCategory': category.Category.objects.all(),
+        'first_row_icons': icons_contact.firstRow,
+        'second_row_icons': icons_contact.secondRow,
     }
     return render(request, 'index.html', context=context)
+
+
+def portfolio(request):
+    context = {}
+
+    return render(request, 'portfolio.html', context=context)
