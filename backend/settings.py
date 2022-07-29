b739ce4414313e0d9d00954eded2ae08f7192433
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-07-29 06:19:37 +0100

    set maximum connection

--- backend/settings.py
+++ backend/settings.py
@@ -17,6 +17,9 @@ from pathlib import Path
 # load environment variables
 load_dotenv()
 
+# set max connection for database
+MAX_CONN_AGE = 600
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
