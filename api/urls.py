fc711918cc80ad3225214665e92c5be26e2ec738
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-08-03 04:25:41 +0100

    added pyscopg binary and route urls on current page

--- api/urls.py
+++ api/urls.py
@@ -1,18 +1,20 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 from api.views import (
-    IndexView, CategoriesView, ContactIconsView, EducationView, ExperienceView,
+    IndexView, CategoriesView, ContactIconsView, EducationView, ExperienceView, PersonalInfoView, SkillView,
     WorkView,
 )
 
 router = SimpleRouter()
-# router.register('', IndexView, basename='index')
 router.register('categories', CategoriesView, basename='category')
 router.register('contactIcons', ContactIconsView, basename='contact icon')
 router.register('education', EducationView, basename='education')
 router.register('experience', ExperienceView, basename='experience')
+router.register('personal_info', PersonalInfoView)
+router.register('skill', SkillView)
 router.register('work', WorkView, 'work')
 
 urlpatterns = [
-    path('', IndexView.as_view())
+    path('', IndexView.as_view()),
+    path('', include(router.urls)),
 ]
