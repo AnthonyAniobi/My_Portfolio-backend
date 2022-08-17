25b1f4aab96b07a130f26294a838c77ce1f4360e
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-08-17 09:27:52 +0100

    removed mock data

--- web/views.py
+++ web/views.py
@@ -2,50 +2,31 @@ from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Context, loader
-# from web.models import (
-#     category, work, experience, education, skill,
-#     personal_info, contact_icons
-#     )
-from web.mock_data import (
-    education, icons_contact, works, experience, personal_info,
-    skills
-)
+from web.models import (
+    category, work, experience, education, skill,
+    personal_info, contact_icons
+    )
 
-# def index(request):
-#     template = loader.get_template("index.html")
-#     return HttpResponse(template.render)
 
 def index(request):
 
-    # context = {
-    #     "personal_info":personal_info.PersonalInfo.objects.first(),
-    #     "codeSkills":skill.Skill.objects.filter(isCoding='True'),
-    #     "designSkills":skill.Skill.objects.filter(isCoding='False'),
-    #     'education': education.Education.objects.all(),
-    #     'experience': experience.Experience.objects.all(),
-    #     'works': work.Work.objects.all(),
-    #     'worksCategory': category.Category.objects.all(),
-    #     'first_row_icons': contact_icons.ContactIcons.objects.filter(first_row='True'),
-    #     'second_row_icons': contact_icons.ContactIcons.objects.filter(first_row='False'),
-    # }
-
     context = {
-        "personal_info":personal_info.PersonalInfo(),
-        "codeSkills":skills.coding,
-        "designSkills":skills.design,
-        'education': education.getAll,
-        'experience': experience.getAll,
-        'works': works.MyWorks.getAll(),
-        'worksCategory': works.MyWorks.categories(),
-        'first_row_icons': icons_contact.IconsContact.firstRow(),
-        'second_row_icons': icons_contact.IconsContact.secondRow(),
+        "personal_info":personal_info.PersonalInfo.objects.first(),
+        "codeSkills":skill.Skill.objects.filter(isCoding='True'),
+        "designSkills":skill.Skill.objects.filter(isCoding='False'),
+        'education': education.Education.objects.all(),
+        'experience': experience.Experience.objects.all(),
+        'works': work.Work.objects.all(),
+        'worksCategory': category.Category.objects.all(),
+        'first_row_icons': contact_icons.ContactIcons.objects.filter(first_row='True'),
+        'second_row_icons': contact_icons.ContactIcons.objects.filter(first_row='False'),
     }
+
 
     return render(request, 'index.html', context=context)
 
 
 def portfolio(request):
-    
     context = {}
     return render(request, 'portfolio.html', context=context)
 
